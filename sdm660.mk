a5d2faeb9b1dc60df673a54fadbcdda4dffa0fe2
Balsz <filecloudrom@gmail.com>
2025-12-03 08:35:58 +0700

    Remove thermal service from product packages
    Removed thermal service package from PRODUCT_PACKAGES.

--- sdm660.mk
+++ sdm660.mk
@@ -514,10 +514,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal-service.qti
-
 # USB
 PRODUCT_PACKAGES += \
     usb_compositions.conf \
